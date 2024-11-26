c65627c63735c89b50cd2239e57ee58435ac0971
Einar W. Høst <einarwh@gmail.com>
2024-11-26 20:45:58 +0100

    Update core.clj

--- src/powerblog/core.clj
+++ src/powerblog/core.clj
@@ -1,8 +1,31 @@
 (ns powerblog.core
   (:require [powerpack.markdown :as md]))
 
+(defn get-blog-posts [db]
+  (->> (d/q '[:find [?e ...]
+              :where
+              [?e :blog-post/author]]
+            db)
+       (map #(d/entity db %))))
+
+(defn render-frontpage [context page]
+  [:html
+   [:head
+    [:title "The Powerblog"]]
+   [:body
+    (md/render-html (:page/body page))
+    [:h2 "Blog posts"]
+    [:ul
+     (for [blog-post (get-blog-posts (:app/db context))]
+       [:li [:a {:href (:page/uri blog-post)} (:page/title blog-post)]])]]])
+
 (defn render-page [context page]
-  (md/render-html (:page/body page)))
+  (cond
+    (= "/" (:page/uri page))
+    (render-frontpage context page)
+
+    :else
+    (md/render-html (:page/body page))))
 
 (def config
   {:datomic/schema-file "resources/schema.edn"
