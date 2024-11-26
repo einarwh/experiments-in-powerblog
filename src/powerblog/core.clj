31db290daf8cd66d0ee20cf8aecfdad21c01d81b
Einar W. Høst <einarwh@gmail.com>
2024-11-26 23:18:41 +0100

    Stuff

--- src/powerblog/core.clj
+++ src/powerblog/core.clj
@@ -5,5 +5,24 @@
 (def config
   {:site/title "The Powerblog"
    :powerpack/render-page #'pages/render-page
-   :powerpack/create-ingest-tx #'ingest/create-tx})
-   
+   :powerpack/create-ingest-tx #'ingest/create-tx
+
+   :optimus/bundles {"app.css"
+                     {:public-dir "public"
+                      :paths ["/styles.css"]}}
+
+   :optimus/assets [{:public-dir "public"
+                     :paths [#".*\.jpg"]}]
+
+   :imagine/config {:prefix "image-assets"
+                    :resource-path "public"
+                    :disk-cache? true
+                    :transformations
+                    {:preview-small
+                     {:transformations [[:fit {:width 184 :height 184}]
+                                        [:crop {:preset :square}]]
+                      :retina-optimized? true
+                      :retina-quality 0.4
+                      :width 184}}}})
+                      
+
---
+++ src/powerblog/export.clj
@@ -0,0 +1,9 @@
+(ns powerblog.export
+  (:require [powerblog.core :as blog]
+            [powerpack.export :as export]))
+
+(defn ^:export export! [& args]
+  (-> blog/config
+      (assoc :site/base-url "https://www.example.com")
+      export/export!))
+      
