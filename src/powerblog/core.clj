8883e2a9872786c47dce50ad20796300faa2ccec
Einar W. Høst <einarwh@gmail.com>
2024-11-26 21:25:45 +0100

    ingest and pages

--- src/powerblog/core.clj
+++ src/powerblog/core.clj
@@ -1,33 +1,9 @@
 (ns powerblog.core
-  (:require [powerpack.markdown :as md]))
-
-(defn get-blog-posts [db]
-  (->> (d/q '[:find [?e ...]
-              :where
-              [?e :blog-post/author]]
-            db)
-       (map #(d/entity db %))))
-
-(defn render-frontpage [context page]
-  [:html
-   [:head
-    [:title "The Powerblog"]]
-   [:body
-    (md/render-html (:page/body page))
-    [:h2 "Blog posts"]
-    [:ul
-     (for [blog-post (get-blog-posts (:app/db context))]
-       [:li [:a {:href (:page/uri blog-post)} (:page/title blog-post)]])]]])
-
-(defn render-page [context page]
-  (cond
-    (= "/" (:page/uri page))
-    (render-frontpage context page)
-
-    :else
-    (md/render-html (:page/body page))))
+  (:require [powerblog.ingest :as ingest]
+            [powerblog.pages :as pages]))
 
 (def config
-  {:datomic/schema-file "resources/schema.edn"
-   :site/title "The Powerblog"
-   :powerpack/render-page #'render-page})
+  {:site/title "The Powerblog"
+   :powerpack/render-page #'pages/render-page
+   :powerpack/create-ingest-tx #'ingest/create-tx})
+   
