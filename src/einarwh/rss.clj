d94c7eac698e0583d2f55978181e2b11ad76f08d
Einar W. Høst <einarwh@gmail.com>
2025-07-16 15:44:38 +0200

    Update rss.clj

--- src/einarwh/rss.clj
+++ src/einarwh/rss.clj
@@ -3,12 +3,20 @@
    [clojure.data.xml :as xml]
    [xmlns.http%3a%2f%2fwww.w3.org%2f2005%2fAtom :as-alias atom]
    [hiccup.core :refer [html]]
-   [powerpack.markdown :as md]))
+   [powerpack.markdown :as md])
+  (:import [java.time ZoneId]
+           [java.time.format DateTimeFormatter]))
+
+(defn time-str [ldt]
+  #_(str (.toOffsetDateTime
+        (.atZone ldt (ZoneId/of "Europe/Oslo"))))
+  (.format (.atZone ldt (ZoneId/of "Europe/Oslo")) DateTimeFormatter/ISO_OFFSET_DATE_TIME))
+
 
 (defn feed-entry [post]
   [::atom/entry
    [::atom/title (:page/title post)]
-   [::atom/updated (:blog-post/published post)]
+   [::atom/updated (time-str (:blog-post/published post))]
    [::atom/author (:person/full-name (:blog-post/author post))]
    [::atom/link {:href (str "https://einarwh.no" (:page/uri post))}]
    [::atom/id (str "urn:einarwh-no:feed:post:" (:page/uri post))]
@@ -22,9 +30,10 @@
 (defn atom-xml [posts]
   (xml/emit-str
    (xml/sexp-as-element 
-      [::atom/feed
+      [::atom/feed {:xmlns "http://www.w3.org/2005/Atom"
+            :xmlns:media "http://search.yahoo.com/mrss/"}
        [::atom/id "urn:einarwh-no:feed"]
-       [::atom/updated (-> posts first :date)]
+       [::atom/updated (time-str (-> posts first :blog-post/published))]
        [::atom/title {:type "text"} "einarwh"]
        [::atom/link {:rel "self" :href "http://einarwh.no/feed/atom.xml"}]
        (map feed-entry posts)])))
